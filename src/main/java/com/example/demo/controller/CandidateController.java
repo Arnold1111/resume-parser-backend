3950e4e39fef14a33ce780fef0e158bc2c617e51
dlwns <dlwns@119.206.17.213>
2026-04-26 09:42:38 +0900

    fix: 프론트엔드 통신 에러 방지를 위한 전역 CORS 허용 추가

--- src/main/java/com/example/demo/controller/CandidateController.java
+++ src/main/java/com/example/demo/controller/CandidateController.java
@@ -3,11 +3,13 @@ package com.example.demo.controller;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/candidates")
 public class CandidateController {
--- src/main/java/com/example/demo/controller/ResumeController.java
+++ src/main/java/com/example/demo/controller/ResumeController.java
@@ -3,11 +3,13 @@ package com.example.demo.controller;
 import java.util.Map;
 import java.util.UUID;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/resumes")
 public class ResumeController {
